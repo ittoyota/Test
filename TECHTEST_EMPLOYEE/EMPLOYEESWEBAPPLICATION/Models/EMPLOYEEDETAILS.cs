97750ba7623e445a89fc74290de632943b7a3b31
ittoyota <54346892+ittoyota@users.noreply.github.com>
2020-02-25 16:43:30 +0500

    adfsf

--- TECHTEST_EMPLOYEE/EMPLOYEESWEBAPPLICATION/Models/EMPLOYEEDETAILS.cs
+++ TECHTEST_EMPLOYEE/EMPLOYEESWEBAPPLICATION/Models/EMPLOYEEDETAILS.cs
@@ -13,11 +13,13 @@ namespace EMPLOYEESWEBAPPLICATION.Models
         public string DESIGNATION { get; set; }
         public Nullable<int> AGE { get; set; }
         public string SALARY { get; set; }
-        [Display(Name = "User Pwd")]
-        [Required(ErrorMessage = "Pwd is required")]
+        [Display(Name = "User Name")]
+        [Required(ErrorMessage = "User Name is required")]
         public string USERNAME { get; set; }
+        //[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [Display(Name = "User Pwd")]
         [Required(ErrorMessage = "Pwd is required")]
+        [DataType(DataType.Password)]
         public string USERPASSWORD { get; set; }
         public string EMPROLE { get; set; }
     }
